d290af2d604a37926e7bd1675be735935e7f348c
John Wilkes <jwilkes0011@gmail.com>
2017-01-28 20:24:46 -0600

    Adding more tests

---
+++ test/test2.s
@@ -0,0 +1,22 @@
+MOV R1 # comment after instruction
+# comment on its own line
+
+# ^ blank line
+ADD R2
+MOV R7
+
+# indentation
+    MOV R1
+    ADD R2
+    MVR0 R1
+
+
+# immediate values
+MOVI 0
+MOVI 1
+MOVI 2
+MOVI 3
+MOVI 4
+MOVI 5
+MOVI 6
+MOVI 7
