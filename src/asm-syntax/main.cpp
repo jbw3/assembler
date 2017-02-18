d520a20697e693dd0b877913a7840c495d3eb45f
John Wilkes <jwilkes0011@gmail.com>
2017-02-18 11:09:25 -0600

    Adding help message

--- src/asm-syntax/main.cpp
+++ src/asm-syntax/main.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -8,6 +9,15 @@
 #include "SyntaxGenerator.h"
 
 using namespace std;
+
+constexpr char HELP_MESSAGE[] = R"(asm-syntax <instruction set> [output file] [-h]
+
+Generate a text editor syntax file for an instruction set. If no output file is
+specified, stdout is used.
+
+Example:
+  asm-syntax W8 W8.tmLanguage
+)";
 
 int main(int argc, const char* argv[])
 {
@@ -20,6 +30,11 @@ int main(int argc, const char* argv[])
     {
         cout << "Too many arguments were given.\n";
         return 1;
+    }
+    else if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
+    {
+        cout << HELP_MESSAGE;
+        return 0;
     }
 
     // get instruction set
