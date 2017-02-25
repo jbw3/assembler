63060403da4531f38d384ee1eaab9f4143b5c34f
John Wilkes <jwilkes0011@gmail.com>
2017-02-24 18:44:29 -0600

    Test class refactor

--- test/test.py
+++ test/test.py
@@ -9,36 +9,32 @@ def asm(iSet, inFilename, outFile, errFile):
     exe = os.path.join(BIN_DIR, 'asm')
     subprocess.run([exe, '--no-color', inFilename, '-i', iSet], stdout=outFile, stderr=errFile)
 
-class Tester(object):
-    def __init__(self):
-        self.numTests = 0
-        self.numPassed = 0
-        self.testArgs = []
-
-    def add(self, iSet, testName):
-        self.testArgs.append((iSet, testName))
-        self.numTests += 1
+class Test(object):
+    def __init__(self, name):
+        self._name = name
 
     def run(self):
-        for args in self.testArgs:
-            iSet, name = args
-            passed = self._runTest(iSet, name)
-            if passed:
-                self.numPassed += 1
-            else:
-                print(name, 'failed.')
+        return False
 
-        print('{} of {} test{} passed.'.format(self.numPassed, self.numTests, '' if self.numTests == 1 else 's'))
+    def getName(self):
+        return self._name
 
-    def _runTest(self, iSet, testName):
-        inFilename = os.path.join(TEST_DIR, testName + '.s')
+    name = property(getName)
+
+class FileTest(Test):
+    def __init__(self, name, iSet):
+        super(FileTest, self).__init__(name)
+        self._iSet = iSet
+
+    def run(self):
+        inFilename = os.path.join(TEST_DIR, self.name + '.s')
         outFilename = os.path.join(TEST_DIR, 'temp.out')
         errFilename = os.path.join(TEST_DIR, 'temp.err')
-        expectedOut = os.path.join(TEST_DIR, testName + '.out')
-        expectedErr = os.path.join(TEST_DIR, testName + '.err')
+        expectedOut = os.path.join(TEST_DIR, self.name + '.out')
+        expectedErr = os.path.join(TEST_DIR, self.name + '.err')
 
         with open(outFilename, 'w') as outFile, open(errFilename, 'w') as errFile:
-            asm(iSet, inFilename, outFile, errFile)
+            asm(self._iSet, inFilename, outFile, errFile)
 
         equal = filecmp.cmp(outFilename, expectedOut, False)
         equal &= filecmp.cmp(errFilename, expectedErr, False)
@@ -54,24 +50,43 @@ class Tester(object):
         except OSError:
             pass
 
+class Tester(object):
+    def __init__(self):
+        self.tests = []
+
+    def add(self, test):
+        self.tests.append(test)
+
+    def run(self):
+        numPassed = 0
+        for test in self.tests:
+            passed = test.run()
+            if passed:
+                numPassed += 1
+            else:
+                print(test.name, 'failed.')
+
+        numTests = len(self.tests)
+        print('{} of {} test{} passed.'.format(numPassed, numTests, '' if numTests == 1 else 's'))
+
 def main():
     tester = Tester()
 
-    tester.add('W8', 'W8_1')
-    tester.add('W8', 'W8_2')
-    tester.add('W16', 'W16_1')
-    tester.add('W16', 'numbers')
-    tester.add('W16', 'invalid_token')
-    tester.add('W16', 'invalid_token_eol')
-    tester.add('W16', 'no_arg_sep')
-    tester.add('W16', 'no_arg')
-    tester.add('W16', 'no_arg_eol')
-    tester.add('W16', 'invalid_instruction')
-    tester.add('W16', 'warn_large_imm')
-    tester.add('W16', 'labels')
-    tester.add('W8', 'warn_large_labels')
-    tester.add('W16', 'redefined_label')
-    tester.add('W16', 'constants')
+    tester.add(FileTest('W8_1', 'W8'))
+    tester.add(FileTest('W8_2', 'W8'))
+    tester.add(FileTest('W16_1', 'W16'))
+    tester.add(FileTest('numbers', 'W16'))
+    tester.add(FileTest('invalid_token', 'W16'))
+    tester.add(FileTest('invalid_token_eol', 'W16'))
+    tester.add(FileTest('no_arg_sep', 'W16'))
+    tester.add(FileTest('no_arg', 'W16'))
+    tester.add(FileTest('no_arg_eol', 'W16'))
+    tester.add(FileTest('invalid_instruction', 'W16'))
+    tester.add(FileTest('warn_large_imm', 'W16'))
+    tester.add(FileTest('labels', 'W16'))
+    tester.add(FileTest('warn_large_labels', 'W8'))
+    tester.add(FileTest('redefined_label', 'W16'))
+    tester.add(FileTest('constants', 'W16'))
 
     tester.run()
 
