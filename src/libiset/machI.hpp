7374fead041ed9bfe2ea655f0c4dfe4ec4f2a9fb
lasserre <cstew33@gmail.com>
2017-03-30 21:06:59 -0500

    Upgraded machI specification to latest API

--- src/libiset/machI.hpp
+++ src/libiset/machI.hpp
@@ -7,56 +7,47 @@ namespace machI {
     const unsigned int OpcodeSize = 6;
     const unsigned int OpcodeOffset = 0;
 
-    const InstructionType regImm21
-    { 
-        OpcodeSize, OpcodeOffset,
-        {
-            {Argument::eRegister, 5, 6},             
-            {Argument::eImmediate, 21, 11} 
-        }
-    };
+    const InstructionType regImm21Type { 6, 5, 21 };
+    const InstructionType regsImm16Type { 6, 5, 5, 16 };
 
-    const InstructionType regImm16
+    const std::initializer_list<Argument> regArgs =
     {
-        OpcodeSize, OpcodeOffset,
-        {
-            { Argument::eRegister, 5, 6 },
-            { Argument::eImmediate, 16, 16 }
-        }
+        { Argument::eRegister, 1 },        
     };
 
-    const InstructionType reg
+    const std::initializer_list<Argument> regRegArgs = 
     {
-        OpcodeSize, OpcodeOffset,
-        {
-            { Argument::eRegister, 5, 6 }            
-        }
+        { Argument::eRegister, 1 },
+        { Argument::eRegister, 2 },
     };
 
-    const InstructionType regReg
+    const std::initializer_list<Argument> regImm21Args =
     {
-        OpcodeSize, OpcodeOffset,
-        {
-            { Argument::eRegister, 5, 6 },
-            { Argument::eRegister, 5, 11 }
-        }
+        { Argument::eRegister, 1 },
+        { Argument::eImmediate, true },     // signed
     };
 
-    const InstructionSet ISET("machI", 32, 
+    const std::initializer_list<Argument> regImm16Args =
+    {
+        { Argument::eRegister, 1 },
+        { Argument::eImmediate, /*signed*/ true, /*shift*/ 0, /*index*/ 3 },
+    };
+
+    const InstructionSet ISET("machI", 32, Endianness::Big,
         {
             { "r", 0, 31, 0b00000 }
         },
         {
-            { "movr", 0x05, regReg },       // move rs into rd
-            { "xor", 0x07, regImm21 },      // xor rd and imm21
-            { "mov", 0x08, regImm21 },      // move imm21 to rd
-            { "add", 0x09, regImm21 },      // add rd and imm21
-            { "jmp", 0x0A, reg },           // jump to address in rd
-            { "out", 0x11, regImm21 },      // output a word from rd into I/O port imm21
-            { "jne", 0x12, reg },           // jump to address in rd if ZF != 1
-            { "movh", 0x16, regImm16 },     // move imm16 to high hw of rd
-            { "movl", 0x18, regImm16 },     // move imm16 to low hw of rd
-            { "cmp", 0x19, regReg },        // compare rd and rs, set flags
+            { "movr", regsImm16Type,    {0x05}, regRegArgs },       // move rs into rd
+            { "xor", regImm21Type,      {0x07}, regImm21Args },     // xor rd and imm21
+            { "mov", regImm21Type,      {0x08}, regImm21Args },     // move imm21 to rd
+            { "add", regImm21Type,      {0x09}, regImm21Args },     // add rd and imm21
+            { "jmp", regImm21Type,      {0x0A}, regArgs },          // jump to address in rd
+            { "out", regImm21Type,      {0x11}, regImm21Args },     // output a word from rd into I/O port imm21
+            { "jne", regImm21Type,      {0x12}, regArgs },          // jump to address in rd if ZF != 1
+            { "movh", regsImm16Type,    {0x16}, regImm16Args },     // move imm16 to high hw of rd
+            { "movl", regsImm16Type,    {0x18}, regImm16Args },     // move imm16 to low hw of rd
+            { "cmp", regsImm16Type,     {0x19}, regRegArgs },       // compare rd and rs, set flags
         }
     );
 }
