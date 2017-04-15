c6ab4fe73909d55a5e876a698caef9846cc35e17
lasserre <cstew33@gmail.com>
2017-04-15 15:28:26 -0500

    Updated jmp to be relative

--- src/libiset/machI.hpp
+++ src/libiset/machI.hpp
@@ -9,6 +9,7 @@ namespace machI {
 
     const InstructionType regImm21Type { 6, 5, 21 };
     const InstructionType regsImm16Type { 6, 5, 5, 16 };
+    const InstructionType imm26Type{ 6, 26 };
 
     const std::initializer_list<Argument> regArgs =
     {
@@ -33,6 +34,11 @@ namespace machI {
         { Argument::eImmediate, /*index*/ 3, /*signed*/ true },
     };
 
+    const std::initializer_list<Argument> imm26Args =
+    {
+        { Argument::eImmediate, /*index*/ 1, /*isSigned*/ true, /*rightShift*/ 2, /*isRelativeAddress*/ true }
+    };
+
     const InstructionSet ISET("machI", 32, Endianness::Big,
         {
             { "r", 0, 31, 0b00000 }
@@ -42,7 +48,8 @@ namespace machI {
             { "xor", regImm21Type,      {0x07}, regImm21Args },     // xor rd and imm21
             { "mov", regImm21Type,      {0x08}, regImm21Args },     // move imm21 to rd
             { "add", regImm21Type,      {0x09}, regImm21Args },     // add rd and imm21
-            { "jmp", regImm21Type,      {0x0A}, regArgs },          // jump to address in rd
+            { "jmpf", regImm21Type,     {0x0A}, regArgs },          // jump to address in rd
+            { "jmp", imm26Type,         {0x0B}, imm26Args },        // jump to relative address (imm26<<2)
             { "out", regImm21Type,      {0x11}, regImm21Args },     // output a word from rd into I/O port imm21
             { "jne", regImm21Type,      {0x12}, regArgs },          // jump to address in rd if ZF != 1
             { "movh", regsImm16Type,    {0x16}, regImm16Args },     // move imm16 to high hw of rd
